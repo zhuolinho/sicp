83c1799c810d0126d393962c3cec9c9a16482ef1
zhuolinho <zhuolinho@hotmail.com>
2019-09-22 23:10:15 +0800

    dot deriv

--- deriv.scm
+++ deriv.scm
@@ -49,22 +49,32 @@
     (and (variable? v1) (variable? v2) (eq? v1 v2))
 )
 
-(define (make-sum a1 a2)
-    (cond 
-        ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))
+(define (make-sum a1 . a2)
+    (if (single-operand? a2)
+        (let ((a2 (car a2)))
+            (cond 
+                ((=number? a1 0) a2)
+                ((=number? a2 0) a1)
+                ((and (number? a1) (number? a2)) (+ a1 a2))
+                (else (list '+ a1 a2))
+            )
+        )
+        (cons '+ (cons a1 a2))
     )
 )
 
-(define (make-product m1 m2)
-    (cond 
-        ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))
+(define (make-product m1 . m2)
+    (if (single-operand? m2)
+        (let ((m2 (car m2)))
+            (cond 
+                ((or (=number? m1 0) (=number? m2 0)) 0)
+                ((=number? m1 1) m2)
+                ((=number? m2 1) m1)
+                ((and (number? m1) (number? m2)) (* m1 m2))
+                (else (list '* m1 m2))
+            )
+        )
+        (cons '* (cons m1 m2))
     )
 )
 
@@ -77,7 +87,12 @@
 )
 
 (define (augend s)
-    (caddr s)
+    (let ((tail-operand (cddr s)))
+        (if (single-operand? tail-operand)
+            (car tail-operand)
+            (apply make-sum tail-operand)
+        )
+    )
 )
 
 (define (product? x)
@@ -89,7 +104,12 @@
 )
 
 (define (multiplicand p)
-    (caddr p)
+    (let ((tail-operand (cddr p)))
+        (if (single-operand? tail-operand)
+            (car tail-operand)
+            (apply make-product tail-operand)
+        )
+    )
 )
 
 (define (=number? exp num)
@@ -114,4 +134,8 @@
         ((= e 1) b)
         (else (list '** b e))
     )
+)
+
+(define (single-operand? lst)
+    (null? (cdr lst))
 )
