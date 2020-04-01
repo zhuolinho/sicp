41afded356ed1daa23c03ea81bc196046014b3d9
zhuolinho <zhuolinho@hotmail.com>
2020-04-01 18:28:45 +0800

    install all package

--- coercion.scm
+++ coercion.scm
@@ -145,8 +145,17 @@
     (define (tag x)
         (attach-tag 'scheme-number x)
     )
-    (put 'exp '(scheme-number scheme-number)
-        (lambda (x y) (tag (expt x y)))
+    (put 'add '(scheme-number scheme-number)
+        (lambda (x y) (tag (+ x y)))
+    )
+    (put 'sub '(scheme-number scheme-number)
+        (lambda (x y) (tag (- x y)))
+    )
+    (put 'mul '(scheme-number scheme-number)
+        (lambda (x y) (tag (* x y)))
+    )
+    (put 'div '(scheme-number scheme-number)
+        (lambda (x y) (tag (/ x y)))
     )
     (put 'make 'scheme-number
         (lambda (x) (tag x))
@@ -157,15 +166,136 @@
     ((get 'make 'scheme-number) n)
 )
 
-(define (make-number n)
-    (attach-tag 'number n)
+(define (install-rational-package)
+    (define (numer x) (car x))
+    (define (denom x) (cdr x))
+    (define (make-rat n d)
+        (let ((g (gcd n d)))
+            (cons (/ n g) (/ d g))
+        )
+    )
+    (define (add-rat x y)
+        (make-rat
+            (+ (* (numer x) (denom y)) (* (numer y) (denom x)))
+            (* (denom x) (denom y))
+        )
+    )
+    (define (sub-rat x y)
+        (make-rat
+            (- (* (numer x) (denom y)) (* (numer y) (denom x)))
+            (* (denom x) (denom y))
+        )
+    )
+    (define (mul-rat x y)
+        (make-rat
+            (* (numer x) (numer y))
+            (* (denom x) (denom y))
+        )
+    )
+    (define (div-rat x y)
+        (make-rat
+            (* (numer x) (denom y))
+            (* (denom x) (numer y))
+        )
+    )
+
+    (define (tag x)
+        (attach-tag 'rational x)
+    )
+    (put 'add '(rational rational)
+        (lambda (x y) (tag (add-rat x y)))
+    )
+    (put 'sub '(rational rational)
+        (lambda (x y) (tag (sub-rat x y)))
+    )
+    (put 'mul '(rational rational)
+        (lambda (x y) (tag (mul-rat x y)))
+    )
+    (put 'div '(rational rational)
+        (lambda (x y) (tag (div-rat x y)))
+    )
+    (put 'make 'rational
+        (lambda (n d) (tag (make-rat n d)))
+    )
+    'done
+)
+(define (make-rational n d)
+    ((get 'make 'scheme-number) n d)
 )
 
-(define (number->scheme-number n) (make-scheme-number (contents n)))
-(put-coercion 'number 'scheme-number number->scheme-number)
+(define (install-complex-package)
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (magnitude z)
+        (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+    (define (angle z)
+        (atan (imag-part z) (real-part z)))
+    ;;; imported procedures from rectangular and polar packages
+    (define (make-from-real-imag x y)
+        ((get 'make-from-real-imag 'rectangular) x y))
 
-(define (scheme-number->number n) (make-number (contents n)))
-(put-coercion 'scheme-number 'number scheme-number->number)
+    (define (make-from-mag-ang r a)
+        ((get 'make-from-mag-ang 'polar) r a))
+
+    ;;; interal procedures
+    (define (add-complex z1 z2)
+        (make-from-real-imag (+ (real-part z1) (real-part z2))
+                             (+ (imag-part z1) (imag-part z2))))
+
+    (define (sub-complex z1 z2)
+        (make-from-real-imag (- (real-part z1) (real-part z2))
+                             (- (imag-part z1) (imag-part z2))))
+
+    (define (mul-complex z1 z2)
+        (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                           (+ (angle z1) (angle z2))))
+
+    (define (div-complex z1 z2)
+        (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                           (- (angle z1) (angle z2))))
+
+    ;;; interface to rest of the system
+    (define (tag z)
+        (attach-tag 'complex z))
+
+    (put 'add '(complex complex)
+        (lambda (z1 z2)
+            (tag (add-complex z1 z2))))
+
+    (put 'sub '(complex complex)
+        (lambda (z1 z2)
+            (tag (sub-complex z1 z2))))
+
+    (put 'mul '(complex complex)
+        (lambda (z1 z2)
+            (tag (mul-complex z1 z2))))
+
+    (put 'div '(complex complex)
+        (lambda (z1 z2)
+            (tag (div-complex z1 z2))))
+
+    (put 'make-from-real-imag 'complex
+        (lambda (x y)
+            (tag (make-from-real-imag x y))))
+
+    (put 'make-from-mag-ang 'complex
+        (lambda (r a)
+            (tag (make-from-mag-ang r a))))
+
+'done)
+(define (make-complex-from-real-imag x y)
+    ((get 'make-from-real-imag 'complex) x y)
+)
+(define (make-complex-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'complex) r a)
+)
+
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
 
 (install-scheme-number-package)
-(apply-generic 'exp (make-number 2) (make-scheme-number 3))
+(apply-generic 'div (make-scheme-number 2) (make-scheme-number 3))
