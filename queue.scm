615552d03d867abc5ab3896bd8017e77f8dc9eaa
zhuolinho <zhuolinho@hotmail.com>
2020-04-21 20:29:53 +0800

    dispatch queue

--- queue.scm
+++ queue.scm
@@ -44,9 +44,49 @@
 (define (print-queue queue)
     (car queue))
 
+(define (make-queue)
+    (let ((front-ptr '()) (rear-ptr '()))
+        (define (empty-queue?)
+            (null? front-ptr))
+
+        (define (front-queue)
+            (if (empty-queue?)
+                (error "Front called with an empty queue")
+                (car front-ptr)))
+        
+        (define (insert-queue! item)
+            (let ((new-pair (list item)))
+                (cond 
+                    ((empty-queue?)
+                        (set! front-ptr new-pair)
+                        (set! rear-ptr new-pair)
+                        front-ptr)
+                    (else 
+                        (set-cdr! rear-ptr new-pair)
+                        (set! rear-ptr new-pair)
+                        front-ptr))))
+
+        (define (delete-queue!)
+            (cond 
+                ((empty-queue?) (error "DELETE! called with an empty queue"))
+                (else 
+                    (set! front-ptr (cdr front-ptr))
+                    front-ptr)))
+
+        (define (dispatch m)
+            (cond 
+                ((eq? m 'insert-queue!) insert-queue!)
+                ((eq? m 'delete-queue!) (delete-queue!))
+                ((eq? m 'empty-queue?) (empty-queue?))
+                ((eq? m 'front-queue) (front-queue))
+                ((eq? m 'print-queue) front-ptr)
+                (else (error "Unknow operation -- DISPATCH" m))))
+
+        dispatch))
+
 (define q1 (make-queue))
-(insert-queue! q1 'a)
-(insert-queue! q1 'b)
-(delete-queue! q1)
-(insert-queue! q1 'c)
-(print-queue q1)
+((q1 'insert-queue!) 'a)
+((q1 'insert-queue!) 'b)
+(q1 'delete-queue!)
+((q1 'insert-queue!) 'c)
+(q1 'print-queue)
