542243b732caed7e520039201ffa347d2bc2cd29
zhuolinho <zhuolinho@hotmail.com>
2020-04-27 11:41:19 +0800

    deque

--- queue.scm
+++ queue.scm
@@ -84,9 +84,89 @@
 
         dispatch))
 
-(define q1 (make-queue))
-((q1 'insert-queue!) 'a)
-((q1 'insert-queue!) 'b)
-(q1 'delete-queue!)
-((q1 'insert-queue!) 'c)
-(q1 'print-queue)
+(define (make-element item)
+    (cons item '()))
+
+(define (get-item ele)
+    (car ele))
+
+(define (pre ele)
+    (cdr ele))
+
+(define (set-pre! ele1 ele2)
+    (set-cdr! ele1 ele2))
+
+(define (make-deque)
+    (cons (list) (list)))
+
+(define (empty-deque? deque)
+    (null? (front-ptr deque)))
+
+(define (front-deque deque)
+    (if (empty-deque? deque)
+        (error "Front called with an empty deque" deque)
+        (car (front-ptr deque))))
+
+(define (rear-deque deque)
+    (if (empty-deque? deque)
+        (error "Rear called with an empty deque" deque)
+        (car (rear-ptr deque))))
+
+(define (front-insert-deque! deque item)
+    (let ((element (make-element item)))
+        (cond 
+            ((empty-deque? deque) 
+                (let ((new-pair (cons element '())))
+                    (set-front-ptr! deque new-pair)
+                    (set-rear-ptr! deque new-pair)
+                    (print-deque deque)))
+            (else 
+                (let ((front (cons element (front-ptr deque))))
+                    (set-pre! (front-deque deque) front)
+                    (set-front-ptr! deque front)
+                    (print-deque deque))))))
+
+(define (rear-insert-deque! deque item)
+    (let ((new-pair (cons (make-element item) '())))
+        (cond 
+            ((empty-deque? deque)
+                (set-front-ptr! deque new-pair)
+                (set-rear-ptr! deque new-pair)
+                (print-deque deque))
+            (else 
+                (set-pre! (car new-pair) (rear-ptr deque))
+                (set-cdr! (rear-ptr deque) new-pair)
+                (set-rear-ptr! deque new-pair)
+                (print-deque deque)))))
+
+(define (front-delete-deque! deque)
+    (cond 
+        ((empty-deque? deque) 
+            (error "DELETE! called with an empty deque" deque))
+        (else 
+            (set-front-ptr! deque (cdr (front-ptr deque)))
+            (set-pre! (front-deque deque) '())
+            (print-deque deque))))
+
+(define (rear-delete-deque! deque)
+    (cond 
+        ((empty-deque? deque) 
+            (error "DELETE! called with an empty deque" deque))
+        (else 
+            (let ((rear (pre (rear-deque deque))))
+                (set-cdr! rear '())
+                (set-rear-ptr! deque rear)
+                (print-deque deque)))))
+
+(define (print-deque deque)
+    (map get-item (car deque)))
+
+(define q (make-deque))
+(front-insert-deque! q 1)
+(rear-insert-deque! q 2)
+(front-delete-deque! q)
+(rear-insert-deque! q 3)
+(rear-delete-deque! q)
+(front-insert-deque! q 4)
+(rear-delete-deque! q)
+(rear-insert-deque! q 5)
