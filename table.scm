2f778ccb146f21e8bb611772fa2b5c2244c98f69
zhuolinho <zhuolinho@hotmail.com>
2020-05-06 16:58:49 +0800

    2-ver table

--- table.scm
+++ table.scm
@@ -41,11 +41,34 @@
 
         dispatch))
 
-(define operation-table (make-table equal?))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
+(define (lookup key-1 key-2 table)
+    (let ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+                (if record
+                    (cdr record)
+                    false))
+            false)))
 
-(put 'a 'b 1)
-(put 'a 'c 2)
-(put 'b 'c 3)
-(get 'a 'c)
+(define (insert! key-1 key-2 value table)
+    (let ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+                (if record
+                    (set-cdr! record value)
+                    (set-cdr! subtable
+                        (cons
+                            (cons key-2 value)
+                            (cdr subtable)))))
+            (set-cdr! table
+                (cons
+                    (list key-1 (cons key-2 value))
+                    (cdr table)))))
+    'ok)
+
+(define (make-table)
+    (list '*table*))
+
+(define t (make-table))
+(insert! 'abc 'cde 123 t)
+(lookup 'abc 'cde t)
