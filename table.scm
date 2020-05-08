11e9fb6c3ac48b6b655c5284d1c38aefe6f1bb6a
zhuolinho <zhuolinho@hotmail.com>
2020-05-08 17:42:14 +0800

    table insert

--- table.scm
+++ table.scm
@@ -58,27 +58,40 @@
                     false)))
         (lookup (list keys) table)))
 
-(define (insert! key-1 key-2 value table)
-    (let ((subtable (assoc key-1 (cdr table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-                (if record
-                    (set-cdr! record value)
-                    (set-cdr! subtable
-                        (cons
-                            (cons key-2 value)
-                            (cdr subtable)))))
-            (set-cdr! table
-                (cons
-                    (list key-1 (cons key-2 value))
-                    (cdr table)))))
+(define (insert! keys value table)
+    ; (let ((subtable (assoc key-1 (cdr table))))
+    ;     (if subtable
+    ;         (let ((record (assoc key-2 (cdr subtable))))
+    ;             (if record
+    ;                 (set-cdr! record value)
+    ;                 (set-cdr! subtable
+    ;                     (cons
+    ;                         (cons key-2 value)
+    ;                         (cdr subtable)))))  
+    ;         (set-cdr! table
+    ;             (cons
+    ;                 (list key-1 (cons key-2 value))
+    ;                 (cdr table)))))
+    (if (list? keys)
+        (if (null? keys)
+            (set-cdr! table value)
+            (let ((subtable (assoc (car keys) (cdr table))))
+                (if subtable
+                    (insert! (cdr keys) value subtable)
+                    (let ((new-table (list (car keys))))
+                        (set-cdr! table
+                            (cons new-table (cdr table)))
+                        (insert! (cdr keys) value new-table)))))
+        (insert! (list keys) value table))
     'ok)
 
 (define (make-table)
     (list '*table*))
 
 (define t (make-table))
-(insert! 'abc 'cde 123 t)
-(insert! 'abc 'fer 456 t)
-(insert! 'awe 'fwe 789 t)
-(lookup (list 'abc) t)
+(insert! 'a-single-key 10086 t)
+(lookup 'a-single-key t)
+(insert! (list 'key-1 'key-2 'key-3) 123 t)
+(lookup (list 'key-1 'key-2 'key-3) t)
+(insert! (list 'key-1 'key-2 'key-3) 'hello-moto t)
+(lookup (list 'key-1 'key-2 'key-3) t)
