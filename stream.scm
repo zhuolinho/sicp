a64c8932396a1c3e2a534ffab0a1e5212ea9f32c
zhuolinho <zhuolinho@hotmail.com>
2020-06-01 14:31:34 +0800

    cons-stream

--- stream.scm
+++ stream.scm
@@ -1,3 +1,23 @@
+(define (delay exp)
+    (lambda () exp))
+
+(define (force delayed-object)
+    (delayed-object))
+
+(define (cons-stream a b)
+    (cons a (delay b)))
+
+(define (stream-car stream)
+    (car stream))
+
+(define (stream-cdr stream)
+    (force (cdr stream)))
+
+(define (stream-ref s n)
+    (if (= n 0)
+        (stream-car s)
+        (stream-ref (stream-cdr s) (- n 1))))
+
 (define (stream-enumerate-interval low high)
     (if (> low high)
         the-empty-stream
