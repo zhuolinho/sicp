cc0b264c1734fa6743e70ba95a0783db87457df0
zhuolinho <zhuolinho@hotmail.com>
2020-06-01 16:04:20 +0800

    memo-proc delay

--- stream.scm
+++ stream.scm
@@ -1,5 +1,15 @@
+(define (memo-proc proc)
+    (let ((already-run? false) (result false))
+        (lambda () 
+            (if (not already-run?)
+                (begin
+                    (set! result (proc))
+                    (set! already-run? true)
+                    result)
+                result))))
+
 (define (delay exp)
-    (lambda () exp))
+    (memo-proc (lambda () exp)))
 
 (define (force delayed-object)
     (delayed-object))
