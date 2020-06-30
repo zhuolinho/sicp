5f02dc213038c4f4cbf992d97e9775175a66a449
zhuolinho <zhuolinho@hotmail.com>
2020-06-30 11:20:13 +0800

    mul-series

--- stream.scm
+++ stream.scm
@@ -156,4 +156,14 @@
 (define sine-series
     (cons-stream 0 (integrate-series cosine-series)))
 
-(stream-head sine-series 10)
+(define (mul-series s1 s2)
+    (cons-stream (* (stream-car s1) (stream-car s2))
+        (add-streams
+            (mul-series s1 (stream-cdr s2))
+            (scale-stream (stream-cdr s1) (stream-car s2)))))
+
+(define one (add-streams
+    (mul-series cosine-series cosine-series)
+    (mul-series sine-series sine-series)))
+
+(stream-head one 1000)
