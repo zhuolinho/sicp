139a9715d1bd7e233c993dba92ce43c35eafb75f
zhuolinho <zhuolinho@hotmail.com>
2020-07-03 14:18:00 +0800

    ln2

--- stream.scm
+++ stream.scm
@@ -214,4 +214,27 @@
         (stream-car (stream-cdr s))
         (stream-limit (stream-cdr s) tolerance)))
 
-(sqrt 9 0.0001)
+(define (ln-summands n)
+    (cons-stream (/ 1.0 n)
+        (stream-map - (ln-summands (+ n 1)))))
+
+(define ln-stream
+    (partial-sums (ln-summands 1)))
+
+(define (euler-transform s)
+    (let ((s0 (stream-ref s 0)) (s1 (stream-ref s 1)) (s2 (stream-ref s 2)))
+        (cons-stream
+            (- s2
+                (/ (square (- s2 s1))
+                    (+ s0 (* -2 s1) s2)))
+            (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+    (cons-stream s
+        (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+    (stream-map stream-car
+        (make-tableau transform s)))
+
+(stream-ref (accelerated-sequence euler-transform ln-stream) 10)
