34a29038f82f5c142c60b961d079b76a87873dbe
zhuolinho <zhuolinho@hotmail.com>
2020-07-13 17:19:04 +0800

    triples

--- stream.scm
+++ stream.scm
@@ -251,5 +251,13 @@
                 (stream-cdr t))
             (pairs (stream-cdr s) (stream-cdr t)))))
 
-(define abc (pairs integers integers))
+(define (triples s t u)
+    (cons-stream (list (stream-car s) (stream-car t) (stream-car u))
+        (interleave 
+            (stream-map
+                (lambda (x) (cons (stream-car s) x))
+                (stream-cdr (pairs t u)))
+            (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
+
+(define abc (triples integers integers integers))
 (stream-head abc 128)
