91f012368dfe5ca976487211a307ca80b95f9001
zhuolinho <zhuolinho@hotmail.com>
2020-07-14 14:26:54 +0800

    weighted-pairs

--- stream.scm
+++ stream.scm
@@ -265,4 +265,54 @@
         (square (caddr lst))))
     (triples integers integers integers)))
 
-(display-stream pythagoras)
+(define (merge-weighted s1 s2 proc)
+    (cond 
+        ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else (let ((s1car (stream-car s1)) (s2car (stream-car s2)))
+            (let ((weight (proc s1car s2car)))
+                (cond
+                    ((< weight 0)
+                        (cons-stream s1car (merge-weighted (stream-cdr s1) s2 proc)))
+                    ((> (proc s1car s2car) 0)
+                        (cons-stream s2car (merge-weighted s1 (stream-cdr s2) proc)))
+                    (else (cons-stream s1car
+                        (merge-weighted (stream-cdr s1) (stream-cdr s2) proc)))))))))
+
+(define (weighted-pairs s t proc)
+    (cons-stream (list (stream-car s) (stream-car t))
+        (merge-weighted 
+            (stream-map
+                (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+            (weighted-pairs (stream-cdr s) (stream-cdr t) proc)
+            proc)))
+
+(define abc (lambda (a b)
+    (let ((sum1 (+ (car a) (cadr a))) (sum2 (+ (car b) (cadr b))))
+        (cond 
+            ((< sum1 sum2) -1)
+            (else 1)))))
+
+(define (def a b)
+    (define (cal lst)
+        (define (remain c)
+            (or 
+                (= (remainder c 2) 0)
+                (= (remainder c 3) 0)
+                (= (remainder c 5) 0)))
+
+        (let ((i (car lst)) (j (cadr lst)))
+            (if (or (remain i) (remain j))
+                (+ (* 2 i) (* 3 j) (* 5 i j))
+                0)))
+
+    (let ((sum1 (cal a)) (sum2 (cal b)))
+        (cond
+            ((and (= sum1 0) (= sum2 0)) 0)
+            ((= sum1 0) 1)
+            ((= sum2 0) -1)
+            ((< sum1 sum2) -1)
+            (else 1))))
+
+(stream-head (weighted-pairs integers integers abc) 128)
