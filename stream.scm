a59cfdacd1219b9ec8c005248fda98087681cb42
zhuolinho <zhuolinho@hotmail.com>
2020-07-20 00:11:56 +0800

    solve & integral

--- stream.scm
+++ stream.scm
@@ -368,9 +368,24 @@
 ;             #f))
 ;     trico)
 
-(stream-each
-    (lambda (s) 
-        (if (= (cube-sum (stream-car s)) (cube-sum (stream-car (stream-cdr s))))
-            (display-line (cube-sum (stream-car s)))
-            #f))
-    ramanujan)
+; (stream-each
+;     (lambda (s) 
+;         (if (= (cube-sum (stream-car s)) (cube-sum (stream-car (stream-cdr s))))
+;             (display-line (cube-sum (stream-car s)))
+;             #f))
+;     ramanujan)
+
+(define (integral delayed-integrand initial-value dt)
+    (define int (cons-stream initial-value
+        (let ((integrand (force delayed-integrand)))
+            (add-streams
+                (scale-stream integrand dt)
+                int))))
+    int)
+
+(define (solve f y0 dt)
+    (define y (integral (delay dy) y0 dt))
+    (define dy (stream-map f y))
+    y)
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
