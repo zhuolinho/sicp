11a6deb7b45e67666c9c1a8a12cfe261060c9cba
zhuolinho <zhuolinho@hotmail.com>
2020-07-21 12:08:51 +0800

    solve-2nd

--- stream.scm
+++ stream.scm
@@ -391,12 +391,10 @@
     (define dy (stream-map f y))
     y)
 
-(define (solve-2nd a b y0 dy0 dt)
+(define (solve-2nd f y0 dy0 dt)
     (define y (integral (delay dy) y0 dt))
     (define dy (integral (delay ddy) dy0 dt))
-    (define ddy (add-streams
-        (scale-stream dy a)
-        (scale-stream y b)))
+    (define ddy (stream-map f dy y))
     y)
 
-(stream-ref (solve-2nd 1 1 1 1 0.001) 1000)
+(stream-ref (solve-2nd (lambda (a b) (+ (* -6 a) (* 7 b))) 1 1 0.001) 1000)
